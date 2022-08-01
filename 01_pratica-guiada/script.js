446c572dc89b5b8014d4b5ec1abc509173a5c392
jeova paiva <jeovappaiva@gmail.com>
2022-08-01 15:50:00 -0300

    a008

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,59 @@
 //CODIGO AQUI
+
+// let saldo = 15
+// let saldoAtualizado = saldo + 22.4
+// console.log(saldoAtualizado)
+
+// saldoAtualizado = saldoAtualizado -  12.34
+// console.log(saldoAtualizado)
+
+// let partePorIrmao = saldoAtualizado / 3
+// console.log(partePorIrmao)
+
+// let minhaParteAplicadaNaBolsa = partePorIrmao * 3.42
+// console.log(minhaParteAplicadaNaBolsa)
+
+// let dinheiroQueRestou = minhaParteAplicadaNaBolsa / 3 
+// console.log(dinheiroQueRestou)
+
+// let operacao = 2 + 2 + 2
+// console.log(operacao)
+
+// let operacaoComPrecedencia = 2 * (2 + 2)
+// console.log(operacaoComPrecedencia)
+
+// const cincoEIgualACinco = 5 === 5
+// const cincoEhDiferenteDeCinco = 5 !== 5 
+// const cincoNumberEhDiferenteDeCincoString = 5 !== "5"
+// const cincoStrigEhIgualDeCincoCorrido = typeof 5 === typeof 20
+// const typeofCincoStringEhIgualTypeofCincoCorrido = typeof "5" === typeof "cinco" 
+ 
+// console.log(cincoEIgualACinco)
+// console.log(incoEhDiferenteDeCinco)
+// console.log(incoNumberEhDiferenteDeCincoString)
+// console.log(cincoStrigEhIgualDeCincoCorrido)
+// console.log(typeofCincoStringEhIgualTypeofCincoCorrido)
+
+// const numero1 = 15
+// const numero2 = 32
+
+// console.log("O primeiro numero é igual ao segundo", numero1 === numero2)
+// console.log("O primeiro numero é menor ou igual ao segundo", numero1 <= numero2)
+// console.log("O primeiro numero é maior ao segundo", numero1 > numero2)
+// console.log("O peimeiro numero é menor que o segundo", numero1 < numero2)
+
+// const A = "Quero dirigir um carro"
+// const B = "sou maior de idade"
+// const C = "Consigo comprar um carro por 22.000"
+// const D = "consigo alugar um carro"
+// const E = "Tenho carteira de motorista"
+
+// a
+// !b
+// b && (!e)
+// b && (c && d)
+// b && e && !a
+
+
+
+
--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,21 @@
 // EXERCICIO 01
-const minhaIdade = prompt('Qual sua idade?')
-const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+const minhaIdade = prompt("Qual sua idade?");
+const idadeAmiga = prompt("Qual a idade da sua amiga ou amigo?");
+
+console.log("Sua idade é maior ou igual ao do seu amigo?", minhaIdade > idadeAmiga);
+
+console.log("A diferença de iadade é:", Number(minhaIdade) - Number(idadeAmiga));
+                                        //(Number(minhaIdade) - Number(idadeAmiga)) * -1); usando "() * -1" eu transformo o numero negativo em positivo ou do contrario.
+
+
 
 // EXERCICIO 02
-const a = 'vou pra praia'
-const b = 'sou maior de idade'
-const c = 'posso tomar uma cervejinha'
-const d = 'quero passear de carro'
+// const a = 'vou pra praia'
+// const b = 'sou maior de idade'
+// const c = 'posso tomar um cerveja
+// const d = 'quero passear de carro'
+
+// !b && d
+// (B && a)
+// a && d
+// c && !d 
